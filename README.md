6b5dd7d0f587ea82e33bb4b87fb4a6b500fbabcd
Bahirul Rizki Ariyanto <ari@detelnetworks.id>
2022-01-30 16:53:30 +0800

    add typing text

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1 align="center">
+    bahirul.github.io
+</h1>
+<p align="center">
+    My fun project.
+</p>
---
+++ main.js
@@ -0,0 +1,30 @@
+const projectsContainer = document.getElementById('projects')
+
+const projects = [
+    {
+        name: "Expanding CSS Card",
+        link: "/expanded-css-card"
+    },
+    {
+        name: "Typing Text JS",
+        link: "/typing-text"
+    }
+]
+
+window.onload = () => {
+    projects.forEach((project) => {
+        const div = document.createElement('div')
+
+        const link = document.createElement('a')
+        link.className = 'project__link'
+        link.href = project.link
+
+        const text = document.createTextNode(project.name)
+
+        // create link
+        link.appendChild(text)
+        div.appendChild(link)
+
+        projectsContainer.appendChild(div)
+    })
+}
